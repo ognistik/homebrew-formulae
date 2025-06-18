1efafb464df5013b2653fac6e6e11ffd842fb5f2
ognistik <github@robertoberg.com>
2025-06-18 16:52:16 -0600

    Pre for release

--- macrowhisper.rb
+++ macrowhisper.rb
@@ -2,7 +2,7 @@ class Macrowhisper < Formula
   desc "Automation helper application for Superwhisper dictation app"
   homepage "https://github.com/ognistik/macrowhisper"
   # url "https://github.com/ognistik/macrowhisper/releases/download/v1.1.0/macrowhisper-1.1.0-macos.tar.gz"
-  url "https://github.com/ognistik/macrowhisper/releases/download/untagged-1829e9f21d85b680ec99/macrowhisper-1.1.0-macos.tar.gz"
+  url "https://github.com/ognistik/macrowhisper/releases/download/v1.1.0/macrowhisper-1.1.0-macos.tar.gz"
   sha256 "9c1b1f04959a7ac713f1f35a8eba5a4c97f6d2dfff193256fec15c5f44e2a6cd"
   license "GPL-3.0"
   head "https://github.com/ognistik/macrowhisper.git", branch: "main"
