e7053062a1159b1b22888e9b6a7617e340a34253
ognistik <github@robertoberg.com>
2025-06-25 13:15:55 -0600

    Binary Update

--- macrowhisper.rb
+++ macrowhisper.rb
@@ -1,8 +1,8 @@
 class Macrowhisper < Formula
   desc "Automation helper application for Superwhisper dictation app"
   homepage "https://github.com/ognistik/macrowhisper"
-  url "https://github.com/ognistik/macrowhisper/releases/download/v1.1.0/macrowhisper-1.1.0-macos.tar.gz"
-  sha256 "93108d86e7f78f5a21373af6885dca4e77a9f15f0bbdb80669bf01215c845168"
+  url "https://github.com/ognistik/macrowhisper/releases/download/v1.1.1/macrowhisper-1.1.1-macos.tar.gz"
+  sha256 "1a068bcad51da2b217f0910b920fc4840a3c91c61fb165c051adf5fa3f5c9782"
   license "GPL-3.0"
   head "https://github.com/ognistik/macrowhisper.git", branch: "main"
 
@@ -15,7 +15,7 @@ class Macrowhisper < Formula
 
   test do
     # Test basic functionality
-    assert_match "macrowhisper version 1.1.0", shell_output("#{bin}/macrowhisper --version")
+    assert_match "macrowhisper version 1.1.1", shell_output("#{bin}/macrowhisper --version")
     
     # Test help command
     assert_match "Usage: macrowhisper", shell_output("#{bin}/macrowhisper --help")
