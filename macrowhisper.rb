ce473ab888797ca952b6d5906a5f865a7e79947c
ognistik <github@robertoberg.com>
2025-08-06 18:02:44 -0600

    Binary Update

--- macrowhisper.rb
+++ macrowhisper.rb
@@ -1,8 +1,8 @@
 class Macrowhisper < Formula
   desc "Automation helper application for Superwhisper dictation app"
   homepage "https://github.com/ognistik/macrowhisper"
-  url "https://github.com/ognistik/macrowhisper/releases/download/v1.2.2/macrowhisper-1.2.2-macos.tar.gz"
-  sha256 "aec1d8bc9a7648559b133f5cade391b61125c9da38160c4aa194796bcdbfa3b6"
+  url "https://github.com/ognistik/macrowhisper/releases/download/v1.2.3/macrowhisper-1.2.3-macos.tar.gz"
+  sha256 "20cb3d71ef11050824e9e9dc68ea16952e35fe7928ed552e02d4d4ccb1f094f9"
   license "GPL-3.0"
   head "https://github.com/ognistik/macrowhisper.git", branch: "main"
 
@@ -11,17 +11,26 @@ class Macrowhisper < Formula
   def install
     # Install the pre-built binary
     bin.install "macrowhisper"
+    
+    # Install the JSON schema file for IDE integration
+    share_dir = share/"macrowhisper"
+    share_dir.mkpath
+    share_dir.install "macrowhisper-schema.json"
   end
 
   test do
     # Test basic functionality
-    assert_match "macrowhisper version 1.2.2", shell_output("#{bin}/macrowhisper --version")
+    assert_match "macrowhisper version 1.2.3", shell_output("#{bin}/macrowhisper --version")
     
     # Test help command
     assert_match "Usage: macrowhisper", shell_output("#{bin}/macrowhisper --help")
     
     # Test config management
     assert_match "Config path", shell_output("#{bin}/macrowhisper --get-config")
+    
+    # Test that schema file is installed
+    schema_path = share/"macrowhisper/macrowhisper-schema.json"
+    assert_predicate schema_path, :exist?, "JSON schema file should be installed"
   end
 
   def caveats
@@ -33,7 +42,7 @@ class Macrowhisper < Formula
       
       2. Verify correct Superwhisper folder path and/or other basic settings
       
-      3.Install as a system service (to run in background):
+      3. Install as a system service (to run in background):
         macrowhisper --start-service
       
       Or run macrowhisper directly:
@@ -43,7 +52,7 @@ class Macrowhisper < Formula
         macrowhisper --restart-service
       
       For more information and documentation, visit:
-      https://github.com/ognistik/macrowhisper
+      https://by.afadingthought.com/macrowhisper
     EOS
   end
 end 
